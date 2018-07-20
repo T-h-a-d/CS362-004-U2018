3201b0b6c21686f32eab30e2218d72f8622e2906
Thad Sauter <thadsauter@gmail.com>
2018-07-20 12:32:59 -0700

    Fix malloc issue

--- projects/sautert/dominion/unittest1.c
+++ projects/sautert/dominion/unittest1.c
@@ -7,9 +7,9 @@
 //Unit test for buyCard
 int main(int argc, char **argv)
 {
-    printf("----Unit Test 1: buyCard----");
+    printf("----Unit Test 1: buyCard----\n");
 
-    struct gameState* state;
+    struct gameState* state = malloc(sizeof(struct gameState*));
     int* cards = kingdomCards(adventurer, smithy, council_room, feast, gardens, mine, remodel, 
                                 village, baron, great_hall);
     
@@ -18,37 +18,45 @@ int main(int argc, char **argv)
     //Make sure player has enough money to buy cards
     state->coins = 100;
 
+    //Test for card in game
+    state->numBuys = 1;
+    printf("Card in Game...");
+    if (buyCard(smithy, state) == -1)
+        printf("Buy Failed\n");
+    else
+        printf("Buy Passed\n");
+
     //Test for card not in game
     state->numBuys = 1;
     printf("Card Not in Game...");
-    if(buyCard(tribute, state))
-        printf("Buy Passed\n");   
-    else
+    if(buyCard(tribute, state) == -1)
         printf("Buy Failed\n");   
+    else
+        printf("Buy Passed\n");   
 
     //Test for none of that type of card left
     state->numBuys = 1;
     state->supplyCount[adventurer] = 0;
     printf("None of that Card Left...");
-    if (buyCard(adventurer, state))
-        printf("Buy Passed\n");
-    else
+    if (buyCard(adventurer, state) == -1)
         printf("Buy Failed\n");
+    else
+        printf("Buy Passed\n");
 
     //Test for no buys left this turn
     state->numBuys = 0;
     printf("No Buys Left this Turn...");
-    if (buyCard(adventurer, state))
-        printf("Buy Passed\n");
-    else
+    if (buyCard(adventurer, state) == -1)
         printf("Buy Failed\n");
+    else
+        printf("Buy Passed\n");
 
     //Test for not enough money
     state->coins = 0;
     state->numBuys = 1;
     printf("Not Enough Money...");
-    if (buyCard(adventurer, state))
-        printf("Buy Passed\n");
-    else
+    if (buyCard(adventurer, state) == -1)
         printf("Buy Failed\n");
+    else
+        printf("Buy Passed\n");
 }
