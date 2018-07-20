947def471aba3069ef25d2b8034468b4b241e28d
Thad Sauter <thadsauter@gmail.com>
2018-07-20 12:16:22 -0700

    buyCard Unit Test

---
+++ projects/sautert/dominion/unittest1.c
@@ -0,0 +1,54 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "assert.h"
+#include "stdio.h"
+
+//Unit test for buyCard
+int main(int argc, char **argv)
+{
+    printf("----Unit Test 1: buyCard----");
+
+    struct gameState* state;
+    int* cards = kingdomCards(adventurer, smithy, council_room, feast, gardens, mine, remodel, 
+                                village, baron, great_hall);
+    
+    initializeGame(2, cards, 100, state);
+
+    //Make sure player has enough money to buy cards
+    state->coins = 100;
+
+    //Test for card not in game
+    state->numBuys = 1;
+    printf("Card Not in Game...");
+    if(buyCard(tribute, state))
+        printf("Buy Passed\n");   
+    else
+        printf("Buy Failed\n");   
+
+    //Test for none of that type of card left
+    state->numBuys = 1;
+    state->supplyCount[adventurer] = 0;
+    printf("None of that Card Left...");
+    if (buyCard(adventurer, state))
+        printf("Buy Passed\n");
+    else
+        printf("Buy Failed\n");
+
+    //Test for no buys left this turn
+    state->numBuys = 0;
+    printf("No Buys Left this Turn...");
+    if (buyCard(adventurer, state))
+        printf("Buy Passed\n");
+    else
+        printf("Buy Failed\n");
+
+    //Test for not enough money
+    state->coins = 0;
+    state->numBuys = 1;
+    printf("Not Enough Money...");
+    if (buyCard(adventurer, state))
+        printf("Buy Passed\n");
+    else
+        printf("Buy Failed\n");
+}
